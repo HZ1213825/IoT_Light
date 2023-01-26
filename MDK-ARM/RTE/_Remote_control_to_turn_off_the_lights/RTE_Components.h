1ef619c4a7496a32b04b0f384df7947207a6585b
HZ1213825 <2635161923@qq.com>
2023-01-26 11:42:28 +0800

    1

---
+++ MDK-ARM/RTE/_Remote_control_to_turn_off_the_lights/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Remote control to turn off the lights' 
+ * Target:  'Remote control to turn off the lights' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/Remote control to turn off the lights/startup_stm32f103x6.d
@@ -0,0 +1,1 @@
+remote control to turn off the lights\startup_stm32f103x6.o: startup_stm32f103x6.s
